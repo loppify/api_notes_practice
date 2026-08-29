69d7c9f6ed9da8f28b00fb21467673bc3a7ef22f
loppify <rost.tarasov@gmail.com>
2026-08-29 12:56:16 +0200

    add swagger route

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from app.api.tasks import router as tasks_router
 from app.api.users import router as users_router
 from app.exception_handlers import register_exception_handlers
 
-app = FastAPI(title="My FastAPI Task App")
+app = FastAPI(title="My FastAPI Task App", docs_url="/docs")
 register_exception_handlers(app)
 app.include_router(tasks_router)
 app.include_router(users_router)
